f202c80f6e1601c4a82556b1a8ea95beb474f71a
antocsan <93600250+antocsan@users.noreply.github.com>
2022-03-10 20:19:56 +0300

    feat:add scroll js

---
+++ scripts/scroll.js
@@ -0,0 +1,25 @@
+const links = document.querySelectorAll('.header-menu__item a')
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const id = element.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+
+        if (section) {
+            seamless.scrollIntoView(section, {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            })
+        } else {
+            seamless.scrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+
+        }
+    })
+})
